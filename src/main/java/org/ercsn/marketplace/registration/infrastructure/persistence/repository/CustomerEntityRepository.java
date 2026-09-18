c9a301927dbbb45cc3a09225d516e125fe3c8539
ercsn <33rcsn@gmail.com>
2026-09-18 01:27:00 -0300

    customer entity repo

---
+++ src/main/java/org/ercsn/marketplace/registration/infrastructure/persistence/repository/CustomerEntityRepository.java
@@ -0,0 +1,10 @@
+package org.ercsn.marketplace.registration.infrastructure.persistence.repository;
+
+
+import org.ercsn.marketplace.registration.infrastructure.persistence.entity.Customer;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.UUID;
+
+public interface CustomerEntityRepository extends CrudRepository<Customer, UUID> {
+}
